d1c9b1211a5b6d9a412a91323bd0c5742e5aa780
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-24 15:38:31 -0400

    Blur editable field upon pressing enter

--- src/Thing.js
+++ src/Thing.js
@@ -14,13 +14,25 @@ class Thing extends Component{
     saveThing(thing);
   }
 
+  blurOnEnter = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      e.target.blur();
+    }
+  }
+
   render() {
     const {thing, removeThing} = this.props;
     return (
       <li className="Thing">
         <input type="checkbox" value="on" />
         <div className="details">
-          <ContentEditable className="name" html={thing.name} onChange={this.updateName} ref={input => this.nameInput = input}/>
+          <ContentEditable
+            className="name" html={thing.name}
+            onChange={this.updateName}
+            ref={input => this.nameInput = input}
+            onKeyPress={this.blurOnEnter}
+          />
           <Actions thing={thing} removeThing={removeThing} />
         </div>
       </li>
