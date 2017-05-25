700191ece05102c12580dccfa401206820e9f5b6
Clayton Lewis <claytonhenrylewis@gmail.com>
2017-05-25 14:42:19 -0400

    Add placeholder signedIn method

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import SignOut from './SignOut';
+import SignIn from './SignIn';
 import ThingList from './ThingList';
 import AddThingButton from './AddThingButton';
 import base from './base';
@@ -22,7 +23,7 @@ class App extends Component {
     },
   }
 
-  render() {
+  renderMain() {
     const actions = {
       saveThing: this.saveThing,
       removeThing: this.removeThing,
@@ -30,12 +31,19 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <Header />
+      <div>
         <SignOut />
         <AddThingButton addThing={this.addThing}/>
         <ThingList things={this.state.things} {...actions}/>
-        
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        {this.signedIn() ? this.renderMain() : <SignIn />}
       </div>
     );
   }
@@ -66,6 +74,10 @@ class App extends Component {
     const things = {...this.state.things};
     things[thing.id] = null;
     this.setState({things});
+  }
+
+  signedIn = () => {
+    return true;
   }
 }
 
